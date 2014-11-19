d048f24b8384924582070c4167472bff9769004c
Chris Micacchi <chris@500px.com>
2014-11-18 20:56:19 -0500

    Fix namespace

--- lib/px/service/errors.rb
+++ lib/px/service/errors.rb
@@ -1,26 +1,28 @@
-module Px::Service
-  ##
-  # Any external service should have its exceptions inherit from this class
-  # so that controllers can handle them all nicely with "service is down" pages or whatnot
-  class ServiceBaseError < StandardError
-    attr_accessor :status
+module Px
+  module Service
+    ##
+    # Any external service should have its exceptions inherit from this class
+    # so that controllers can handle them all nicely with "service is down" pages or whatnot
+    class ServiceBaseError < StandardError
+      attr_accessor :status
 
-    def initialize(message, status)
-      self.status = status
-      super(message)
+      def initialize(message, status)
+        self.status = status
+        super(message)
+      end
     end
-  end
 
-  ##
-  # Indicates something was wrong with the request (ie, not a service failure, but an error on the caller's
-  # part).  Corresponds to HTTP status 4xx responses
-  class ServiceRequestError < ServiceBaseError
-  end
+    ##
+    # Indicates something was wrong with the request (ie, not a service failure, but an error on the caller's
+    # part).  Corresponds to HTTP status 4xx responses
+    class ServiceRequestError < ServiceBaseError
+    end
 
-  ##
-  # Indicates something went wrong during request processing (a service or network error occurred)
-  # Corresponds to HTTP status 5xx responses.
-  # Services should catch other network/transport errors and raise this exception instead.
-  class ServiceError < ServiceBaseError
+    ##
+    # Indicates something went wrong during request processing (a service or network error occurred)
+    # Corresponds to HTTP status 5xx responses.
+    # Services should catch other network/transport errors and raise this exception instead.
+    class ServiceError < ServiceBaseError
+    end
   end
 end
