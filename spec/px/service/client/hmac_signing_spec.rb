24b7600ab824d140a3f77c15f5372383e139c53b
Junji Zhi <junji@500px.com>
2016-03-31 12:08:54 -0400

    Add tests

--- spec/px/service/client/hmac_signing_spec.rb
+++ spec/px/service/client/hmac_signing_spec.rb
@@ -1,16 +1,28 @@
 require 'spec_helper'
 
 describe Px::Service::Client::HmacSigning do
-  subject {
-    Px::Service::Client::Base.tap do |c|
+  let(:subject_class) {
+    Class.new(Px::Service::Client::Base).tap do |c|
+      # Anonymous classes don't have a name.  Stub out :name so that things work
+      allow(c).to receive(:name).and_return("HmacSigning")
       c.include(Px::Service::Client::HmacSigning)
-    end.new
+    end
   }
-
+  
+  let(:another_class) {
+    Class.new(Px::Service::Client::Base).include(Px::Service::Client::HmacSigning).tap do |c|
+      c.hmac_signing do |signing_config|
+        signing_config.secret = "different secret"
+      end
+    end
+  }
+  
+  subject { subject_class.new }
+  let(:another_object) { another_class.new }
+  
   describe '#make_request' do
     context "when the underlying request method succeeds" do
       let(:url) { 'http://localhost:3000/path' }      
-
       let(:resp) { subject.send(:make_request, 'get', url) }
       let(:headers) { resp.request.options[:headers] }
 
@@ -20,6 +32,13 @@ describe Px::Service::Client::HmacSigning do
 
       it "contains a header with auth signature" do
         expect(headers).to have_key("X-Service-Auth")
+        expect(headers).to have_key("Timestamp")
+      end
+      
+      let(:resp2) { another_object.send(:make_request, 'get', url) }
+      let(:headers2) { resp2.request.options[:headers] }
+      it "is different from the object of another class with a different key" do
+        expect(headers["X-Service-Auth"]).not_to eq(headers2["X-Service-Auth"])
       end
     end
   end
